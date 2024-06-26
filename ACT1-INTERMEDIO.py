a193eb63775b81f21f0eec390d670211f579628b
LuissPerezz <Luisyyimi1411@gmail.com>
2024-06-26 08:40:37 -0300

    Create ACT1-INTERMEDIO.py

---
+++ ACT1-INTERMEDIO.py
@@ -0,0 +1,10 @@
+cristales_preciosos = ["Zafiro", "Diamante", "Lapislazuli", "Oro","Diamante"]
+
+
+cristales_unicos = set(cristales_preciosos)
+
+
+cristales_unicos_lista = list(cristales_unicos)
+
+print("Lista de cristales preciosos original:", cristales_preciosos)
+print("Lista de cristales preciosos únicos:", cristales_unicos_lista)
